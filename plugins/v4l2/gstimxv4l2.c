4e0ceeb1fb341192cfac1f111013fd195f1b57c8
Jian Li <jian.li@freescale.com>
2014-04-09 13:55:43 +0800

    [v4l2sink] adjust the buffer required in v4l2sink
    - adjust the buffer required as stream on count
    - change PXP stream on count to 1
    
    Signed-off-by: Jian Li <jian.li@freescale.com>

--- plugins/v4l2/gstimxv4l2.c
+++ plugins/v4l2/gstimxv4l2.c
@@ -38,9 +38,8 @@ GST_DEBUG_CATEGORY_EXTERN (imxv4l2_debug);
 #define GST_CAT_DEFAULT imxv4l2_debug
 
 
-#define V4L2_HOLDED_BUFFERS (2)
 #define MX6Q_STREAMON_COUNT (1)
-#define MX60_STREAMON_COUNT (2)
+#define MX60_STREAMON_COUNT (1)
 
 #define MAX_BUFFER (32)
 #define UPALIGNTO8(a) ((a + 7) & (~7))
@@ -350,14 +349,14 @@ gst_imx_v4l2_get_min_buffer_num (gint type)
   gint num;
   if (type == V4L2_BUF_TYPE_VIDEO_OUTPUT) {
     if (gimx_chip == CC_MX60)
-      num = MAX (V4L2_HOLDED_BUFFERS, MX60_STREAMON_COUNT);
+      num = MX60_STREAMON_COUNT;
     else if (gimx_chip == CC_MX6Q)
-      num = MAX (V4L2_HOLDED_BUFFERS, MX60_STREAMON_COUNT);
+      num =  MX6Q_STREAMON_COUNT;
     else 
-      num = V4L2_HOLDED_BUFFERS;
+      num = 1;
 
-    //add extra 3 buffers for better performance
-    num += 3;
+    //add extra 2 buffers for better performance
+    num += 2;
   }
 
   return num;
@@ -1337,7 +1336,7 @@ gint gst_imx_v4l2_dequeue_buffer (gpointer v4l2handle, GstBuffer **buffer)
   struct v4l2_buffer v4l2buf;
   gint trycnt = 0;
 
-  if (handle->queued_count <= MAX(V4L2_HOLDED_BUFFERS, handle->streamon_count))
+  if (handle->queued_count <= handle->streamon_count)
     return 0;
 
   if (handle->invisible) {
