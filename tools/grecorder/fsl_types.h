a490ddb7ad846377d6f61c8a21b7fbc1478abc01
Song Bing <b06498@freescale.com>
2015-03-19 10:50:46 +0800

    MMFMWK-6380 Camera engine development
    Update copyright to LGPL.
    
    Signed-off-by: Song Bing <b06498@freescale.com>

--- tools/grecorder/fsl_types.h
+++ tools/grecorder/fsl_types.h
@@ -1,10 +1,21 @@
 /*
-***********************************************************************
-* Copyright 2014 by Freescale Semiconductor, Inc.
-* All modifications are confidential and proprietary information
-* of Freescale Semiconductor, Inc. ALL RIGHTS RESERVED.
-***********************************************************************
-*/
+ * Copyright (c) 2014-2015, Freescale Semiconductor, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #ifndef _FSL_TYPES_H
 #define _FSL_TYPES_H
--- tools/grecorder/recorder_engine.h
+++ tools/grecorder/recorder_engine.h
@@ -1,10 +1,20 @@
-/**
- *  Copyright (c) 2014, Freescale Semiconductor Inc.,
- *  All Rights Reserved.
+/*
+ * Copyright (c) 2014-2015, Freescale Semiconductor, Inc. All rights reserved.
  *
- *  The following programs are the sole property of Freescale Semiconductor Inc.,
- *  and contain its proprietary and confidential information.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
 #ifndef _RECORDER_ENGINE_h_
