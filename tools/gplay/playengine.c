a251684361cad503cb757d9376b6813c268ac50a
Haihua Hu <jared.hu@nxp.com>
2019-03-07 14:29:46 +0800

    MLK-21037 update copyright for imx-gst1.0-plugins package
    Copyright update to LGPL license in following files:
    tools/gplay/playengine.c
    tools/gplay/playengine.h
    
    Copyrights updated to 2019 in following files:
    tools/gplay2/gplay2.c
    plugins/aiurdemux/aiurdemux.c
    
    Signed-off-by: Haihua Hu <jared.hu@nxp.com>

--- tools/gplay/playengine.c
+++ tools/gplay/playengine.c
@@ -1,11 +1,24 @@
 /**
-*  Copyright (c) 2014-2015, Freescale Semiconductor Inc.,
-*  All Rights Reserved.
-*
-*  The following programs are the sole property of Freescale Semiconductor Inc.,
-*  and contain its proprietary and confidential information.
-*
-*/
+ * Copyright (c) 2014-2015, Freescale Semiconductor Inc.,
+ * Copyright 2019 NXP
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your pconfigion) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 // for fullscreen setting
 #include <fcntl.h>
